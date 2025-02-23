c89bdedd85dc02df8fdecf78fa75952b0edc4974
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2025-02-22 17:09:34 -0800

    feat(tf): add new tf block with required providers

---
+++ terraform/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~>2.0"
+    }
+  }
+}
