e7d5d9f1da932fd1e408dabe08a4eaa1d9b50883
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2025-02-22 17:36:26 -0800

    feat(tf.azure.provider): add variables for provider configuration
    Will read from ENV if set, otherwise will use tfvar

--- terraform/azurerm-provider.tf
+++ terraform/azurerm-provider.tf
@@ -1,3 +1,33 @@
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+  default     = null # Allows fallback to environment variable
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID (Service Principal)"
+  default     = null
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+  default     = null
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret (Service Principal)"
+  sensitive   = true
+  default     = null
+}
+
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
