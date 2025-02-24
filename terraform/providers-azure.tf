9bfb8dc2bcd9f468f14fe195ccf13f3710cee800
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2025-02-24 13:51:33 -0800

    docs(tf): add comments with env fallback values

--- terraform/providers-azure.tf
+++ terraform/providers-azure.tf
@@ -1,26 +1,26 @@
 variable "subscription_id" {
   type        = string
   description = "Azure Subscription ID"
-  default     = null # Allows fallback to environment variable
+  default     = null # Will fall back to ARM_SUBSCRIPTION_ID
 }
 
 variable "client_id" {
   type        = string
   description = "Azure Client ID (Service Principal)"
-  default     = null
+  default     = null # Will fall back to ARM_CLIENT_ID
 }
 
 variable "tenant_id" {
   type        = string
   description = "Azure Tenant ID"
-  default     = null
+  default     = null # Will fall back to ARM_TENANT_ID
 }
 
 variable "client_secret" {
   type        = string
   description = "Azure Client Secret (Service Principal)"
   sensitive   = true
-  default     = null
+  default     = null # Will fall back to ARM_CLIENT_SECRET
 }
 
 provider "azurerm" {
