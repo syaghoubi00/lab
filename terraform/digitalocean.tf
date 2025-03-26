495a683e03e6e10a9dfb254cfb14262f5273e6af
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2025-03-25 18:34:09 -0700

    feat(tf.digitalocean)!: use root user for provisioning

--- terraform/digitalocean.tf
+++ terraform/digitalocean.tf
@@ -10,26 +10,25 @@ resource "digitalocean_project" "prod_lab" {
   environment = "Production"
 }
 
-# resource "digitalocean_ssh_key" "default" {
-#   name       = "TF"
-#   public_key = var.ssh_public_key
-#   # public_key = file("/Users/terraform/.ssh/id_rsa.pub")
-# }
+resource "digitalocean_ssh_key" "default" {
+  name       = "TF"
+  public_key = data.hcp_vault_secrets_app.lab.secrets["ssh_public_key"]
+}
 
 resource "digitalocean_droplet" "docker" {
-  image      = "ubuntu-24-04-x64"
-  name       = "docker"
-  region     = "sfo2"
-  size       = "s-1vcpu-1gb"
-  backups    = true
-  monitoring = true
-  ipv6       = true
-  # ssh_keys    = [digitalocean_ssh_key.default.fingerprint]
+  image       = "ubuntu-24-04-x64"
+  name        = "docker"
+  region      = "sfo2"
+  size        = "s-1vcpu-1gb"
+  backups     = true
+  monitoring  = true
+  ipv6        = true
+  ssh_keys    = [digitalocean_ssh_key.default.fingerprint]
   resize_disk = false
   tags        = ["lab", "docker"]
-  user_data = templatefile("./cloud-init-templates/ansible/user-data.tftpl", {
-    ssh_authorized_keys = [var.ssh_public_key],
-  })
+  # user_data = templatefile("./cloud-init-templates/ansible/user-data.tftpl", {
+  #   ssh_authorized_keys = [var.ssh_public_key],
+  # })
   droplet_agent     = false
   graceful_shutdown = true
 }
@@ -46,10 +45,11 @@ resource "ansible_playbook" "docker_setup" {
   # Extra variables to pass to the playbook
   extra_vars = {
     ansible_host                 = digitalocean_droplet.docker.ipv4_address
-    ansible_user                 = "ansible"
     ansible_ssh_private_key_file = var.ssh_private_key_path
+    ansible_user                 = "root"
 
-    docker_rootless_mode = true
+    docker_rootless_mode  = true
+    docker_ssh_public_key = data.hcp_vault_secrets_app.lab.secrets["ssh_public_key"]
   }
 }
 
