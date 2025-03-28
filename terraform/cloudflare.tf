e534670c698d803f971ddacd90609a316a9df21a
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2025-03-28 15:13:22 -0700

    fix(tf.cloudflare): redirect fqdn to pages domain

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -6,9 +6,9 @@ data "cloudflare_zone" "zags_dev" {
 
 resource "cloudflare_dns_record" "www_zags_dev" {
   zone_id = data.cloudflare_zone.zags_dev.zone_id
-  comment = "Redirect www to root domain"
-  content = "zags.dev"
-  name    = "www"
+  comment = "TF: Redirect www to root domain"
+  content = "zags.pages.dev" # NOTE: 2025-03-28 - CNAME chaining to zags.dev wasn't working
+  name    = "www.zags.dev"   # NOTE: 2025-03-28 - non-FQDN was triggering a resource update
   proxied = true
   ttl     = 1 # 1 is automatic TTL
   type    = "CNAME"
