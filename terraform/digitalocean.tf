8f33e9ecbdd8e9a6ab1d3f602fc80aed368f863d
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2025-03-29 17:26:39 -0700

    feat(tf.docker)!: add vps to inventory file instead of running playbook

--- terraform/digitalocean.tf
+++ terraform/digitalocean.tf
@@ -26,33 +26,23 @@ resource "digitalocean_droplet" "docker" {
   ssh_keys    = [digitalocean_ssh_key.default.fingerprint]
   resize_disk = false
   tags        = ["lab", "docker"]
-  # user_data = templatefile("./cloud-init-templates/ansible/user-data.tftpl", {
-  #   ssh_authorized_keys = [var.ssh_public_key],
-  # })
+  user_data = templatefile("./cloud-init-templates/ansible/user-data.tftpl", {
+    ssh_authorized_keys = [data.hcp_vault_secrets_app.lab.secrets["ssh_public_key"]]
+  })
   droplet_agent     = false
   graceful_shutdown = true
 }
 
-resource "ansible_playbook" "docker_setup" {
-  name     = digitalocean_droplet.docker.ipv4_address
-  playbook = "./playbooks/docker_setup.yaml"
 
-  replayable = false
+resource "ansible_host" "do_docker" {
+  name   = digitalocean_droplet.docker.ipv4_address
+  groups = ["docker", "harden"]
 
-  # Wait for the droplet to be fully provisioned
-  depends_on = [digitalocean_droplet.docker]
-
-  # Extra variables to pass to the playbook
-  extra_vars = {
-    ansible_host                 = digitalocean_droplet.docker.ipv4_address
-    ansible_user                 = "root"
+  variables = {
+    ansible_user                 = "ansible"
     ansible_ssh_private_key_file = local_sensitive_file.ssh_private_key.filename
 
     docker_rootless_mode  = true
     docker_ssh_public_key = data.hcp_vault_secrets_app.lab.secrets["ssh_public_key"]
   }
-}
-
-output "ansible_docker_setup" {
-  value = ansible_playbook.docker_setup.ansible_playbook_stdout
 }
